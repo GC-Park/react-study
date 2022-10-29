b0e677f47fb41cc376aea5b70c4a173da396e86e
GC-Park <kuyt3122@naver.com>
2022-10-29 22:03:58 +0900

    Feat:first commit

---
+++ src/pages/Favorites.js
@@ -0,0 +1,5 @@
+function FavoritesPage(){
+  return <div>FavoritesPage Pages</div>
+}
+
+export default FavoritesPage
---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,5 @@
+function NewMeetupPage(){
+  return <div>NewMeetupPage Pages</div>
+}
+
+export default NewMeetupPage
