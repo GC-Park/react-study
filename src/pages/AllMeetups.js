1769f7409eb4b87ea1f295667d7356b18c36155f
GC-Park <kuyt3122@naver.com>
2022-11-03 20:52:11 +0900

    Feat:데이터베이스에서 데이터 가져오는 기능 추가

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,21 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import MeetupList from '../components/meetups/MeetupList'
 
 function AllMeetupsPage() {
-    const [isLoading, setIsLoading] = useState(true);
-    const [loadingMeetups, setLoadedMeetups] = useState([]);
+    const [isLoading, setIsLoading] = useState(true)
+    const [loadingMeetups, setLoadedMeetups] = useState([])
 
-    fetch(
-        'https://react-getting-started-bfac2-default-rtdb.firebaseio.com/meetups.json'
-    ).then(response => {
-        return response.json
-    }).then(data => {
-        setIsLoading(false)
-        setLoadedMeetups(data)
-    });
+    useEffect(() => {
+        setIsLoading(true)
+        fetch(
+            'https://react-getting-started-bfac2-default-rtdb.firebaseio.com/meetups.json'
+        )
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                const meetups = []
 
-    if(isLoading){
+                for (const key in data) {
+                    const meetup = {
+                        id: key,
+                        ...data[key],
+                    }
+
+                    meetups.push(meetup)
+                }
+
+                setIsLoading(false)
+                setLoadedMeetups(meetups)
+            })
+    }, [])
+
+    if (isLoading) {
         return (
             <section>
                 <p>Loading...</p>
